f1015c0da784e4e840c93fff708307f79faf4558
taesubyun <taesubyun@icloud.com>
2022-10-18 01:32:32 +0900

    Realtime database with Firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-add-backend-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
